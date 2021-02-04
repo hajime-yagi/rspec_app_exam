aeeb294b8394c64dec81e2f0fbde8219305479e1
hajime <touken1996@yahoo.co.jp>
2021-02-04 16:24:00 +0900

    task_index 修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -13,11 +13,11 @@ RSpec.describe 'Task', type: :system do
         expect(current_path).to eq project_tasks_path(project)
       end
 
-      it 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
+      fit 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
         # FIXME: テストが失敗するので修正してください
         visit project_path(project)
         click_link 'View Todos'
-        visit project_tasks_path(project)
+        switch_to_window(windows.last)
         expect(page).to have_content task.title
         expect(Task.count).to eq 1
         expect(current_path).to eq project_tasks_path(project)
