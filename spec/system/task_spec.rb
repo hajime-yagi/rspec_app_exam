650d58ba15319cdcc499948297fba79bcd557ab7
hajime <touken1996@yahoo.co.jp>
2021-02-04 17:11:41 +0900

    association 追記

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
   let(:project){ create(:project)}
-  let(:task) { create(:task, project_id: project.id)}
+  let(:task) { create(:task)}
   describe 'Task一覧' do
     context '正常系' do
       it '一覧ページにアクセスした場合、Taskが表示されること' do
@@ -24,7 +24,6 @@ RSpec.describe 'Task', type: :system do
       end
     end
   end
-
   describe 'Task新規作成' do
     context '正常系' do
       it 'Taskが新規作成されること' do
@@ -88,7 +87,7 @@ RSpec.describe 'Task', type: :system do
     end
   end
   describe 'Task削除' do
-    let!(:task){ create(:task, project_id: project.id) }
+    let!(:task){ create(:task) }
     context '正常系' do
       # FIXME: テストが失敗するので修正してください
       it 'Taskが削除されること' do
